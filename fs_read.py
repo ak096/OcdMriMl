5b17b715b8f746964ca6fee366fff0d19919d275
Akshay <akshay913@gmail.com>
2019-04-02 18:03:30 +0200

    change name group_table to group_frame for naming convention consistency remove removal of constant features - could still be difference between con and pat for t test. and when only using pat feats for ml the constant feats won't be influential add back sort of columns for con and pat comparison and t test

--- fs_read.py
+++ fs_read.py
@@ -5,11 +5,14 @@ import os
 
 def fs_data_collect(group, path_base):
 
-    # subprocess.Popen(['/fs_gen_tables %s' % group], shell=True, executable="/bin/bash")
-    group_table = pd.DataFrame()
+    #subprocess.Popen(['/home/bran/PycharmProjects/OcdMriMl/bash_scripts/fs_gen_tables.sh %s' % group], \
+    #                 shell=True, executable="/bin/bash")
+    group_frame = pd.DataFrame()
 
     # concat fs table files into one data frame
     for table_file in os.scandir(path_base + group + '/' + group + '_tables/'):
+        print("reading %s" % table_file.path)
+
         table = pd.read_table(table_file.path, index_col=0)
 
         # specify feature set (columns)
@@ -21,20 +24,23 @@ def fs_data_collect(group, path_base):
             suffix = 'aparc.a2009s'
         else:
             suffix = 'aparc'
-        table.columns = [n + '**' + suffix if n != 'BrainSegVolNotVent' and n != 'eTIV' else n for n in table.columns]
+        table.columns = [n + '**' + suffix if (n != 'BrainSegVolNotVent' and n != 'eTIV') else n for n in table.columns]
 
         table.columns = [s.replace('_and_', '&') for s in table.columns]
 
         # name the subject index
         table.index.name = 'subj'
 
-        frames = [group_table, table]
-        group_table = pd.concat(frames, axis=1)
+        frames = [group_frame, table]
+        group_frame = pd.concat(frames, axis=1)
 
-    # remove duplicates (BrainSegVolNotVent and eTIV features)
-    group_table = group_table.loc[:, ~group_table.columns.duplicated()]
+    # remove duplicates (specifically BrainSegVolNotVent and eTIV features)
+    group_frame = group_frame.loc[:, ~group_frame.columns.duplicated()]
+
+    # sort by column name
+    group_frame = group_frame.reindex(sorted(group_frame.columns), axis=1)
 
     # remove constant features (f.e. all 0's)
-    group_table = group_table.loc[:, group_table.nunique() != 1]
+    # group_frame = group_frame.loc[:, group_frame.nunique() != 1]
 
-    return group_table
+    return group_frame
